d8f450d5be3471e0ddd9a1f70ff1934a819912ec
guy_schneerson <guy@blue-bag.com>
2016-04-05 16:35:06 +0100

    Issue #2700511: Need a congiguration entity holding metadata needed for stock checking

---
+++ src/Entity/EntityStockConfigurationInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\commerce_stock\Entity\EntityStockConfigurationInterface.
+ */
+
+namespace Drupal\commerce_stock\Entity;
+
+/**
+ * Defines a common interface for configuration.
+ */
+interface EntityStockConfigurationInterface {
+
+
+  /**
+   * Get a list of location relevent for the provided product.
+   *
+   * The product can be ignored. Any other contextual information like active
+   * store/department/.. needs to be managed by the implementing class.
+   *
+   * @return array
+   *   Of location IDs.
+   */
+  public function getLocationList($product_id);
+
+}
