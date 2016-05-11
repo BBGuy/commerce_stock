8bb070acab0d30cd9e8495299c5afe4397f03e64
guy_schneerson <guy@blue-bag.com>
2016-05-11 17:42:12 +0100

    Issue #2720277: High level Stock Transactions - updated StockTransactionsInterface

--- src/StockTransactionsInterface.php
+++ src/StockTransactionsInterface.php
@@ -5,6 +5,13 @@
  * Contains \Drupal\commerce_stock\StockTransactionsInterface.
  */
 
+define('TRANSACTION_TYPE_STOCK_IN', 1);
+define('TRANSACTION_TYPE_STOCK_OUT', 2);
+define('TRANSACTION_TYPE_STOCK_MOVMENT', 3);
+define('TRANSACTION_TYPE_SALE', 4);
+define('TRANSACTION_TYPE_RETURN', 5);
+define('TRANSACTION_TYPE_NEW_STOCK', 6);
+
 namespace Drupal\commerce_stock;
 
 /**
@@ -15,22 +22,22 @@ interface StockTransactionsInterface {
   /**
    * Receive stock.
    */
-  public function receiveStock($variation_id, $location_id, $zone, $quantity, $unit_cost);
+  public function receiveStock($purchasable_entity, $location_id, $zone, $quantity, $unit_cost, $message = NULL);
 
   /**
    * Sell stock.
    */
-  public function sellStock($variation_id, $location_id, $zone, $quantity, $unit_cost, $order_id, $user_id);
+  public function sellStock($purchasable_entity, $location_id, $zone, $quantity, $unit_cost, $order_id, $user_id, $message = NULL);
 
   /**
    * Move stock.
    */
-  public function moveStock($variation_id, $from_location_id, $to_location_id, $from_zone, $to_zone, $quantity, $unit_cost);
+  public function moveStock($purchasable_entity, $from_location_id, $to_location_id, $from_zone, $to_zone, $quantity, $unit_cost, $message = NULL);
 
   /**
    * Stock returns.
    */
-  public function returnStock($variation_id, $location_id,  $zone, $quantity, $unit_cost, $order_id, $user_id);
+  public function returnStock($purchasable_entity, $location_id,  $zone, $quantity, $unit_cost, $order_id, $user_id, $message = NULL);
 
 
 }
