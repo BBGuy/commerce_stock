be8301ac180325948bf0867bba10114cfa66e84b
Steve Oliver <steve@circatree.com>
2016-12-27 17:15:22 -0800

    Try this approach to coverage reporting.

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -6,5 +6,11 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
+# Create directory for coverage results file.
+if [ -n "$DRUPAL_TI_COVERAGE_FILE" ]
+then
+  mkdir -p $(dirname "$DRUPAL_TI_COVERAGE_FILE")
+fi
+
 # Turn on PhantomJS for functional Javascript tests
 phantomjs --ssl-protocol=any --ignore-ssl-errors=true $DRUPAL_TI_DRUPAL_DIR/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
