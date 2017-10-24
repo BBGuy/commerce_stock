4cc1b03b641656ce39dd397a4a81aec876778da7
renatog <renatog@3326031.no-reply.drupal.org>
2017-10-24 16:12:34 +0100

    Issue #2832534 by RenatoG, cebasqueira: Coding Standards

--- commerce_stock.api.php
+++ commerce_stock.api.php
@@ -5,15 +5,16 @@
  * Hooks provided by the Commerce Stock module.
  */
 
-
 /**
+ * Hook cart product level alter.
+ *
  * Allows modules to return a cart product level for a product id before the
  * Commerce Stock module determines it using its default queries.
  *
- * @param $product_id
+ * @param string $product_id
  *   The product_id of the product whose cart quantity should be returned.
  *
- * @return
+ * @return mixed
  *   The cart quantity (if a valid quantity was found), FALSE (if the product
  *   should have no cart product level), or NULL (if the implementation cannot
  *   tell if the product has a cart product level or not).
