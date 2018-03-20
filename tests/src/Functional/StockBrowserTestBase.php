0854201a147bff51f3d9b89cc5e891fe6664a974
olafkarsten <olafkarsten@352745.no-reply.drupal.org>
2018-03-20 06:48:18 +0100

    Issue #2953828 by olafkarsten: Fix broken event test
    - Changed the OrderEventTransactionsTest to a kernel test.
    - Its much faster and easier to debug. We want to test that the correct
      events are captured and proper stock transactions are
      recorded and not if the order admin form works.
    - This covers all methods of the eventHandler.
    - One test still fails due to a bug
      @see https://www.drupal.org/project/commerce_stock/issues/2954152

--- tests/src/Functional/StockBrowserTestBase.php
+++ tests/src/Functional/StockBrowserTestBase.php
@@ -12,6 +12,8 @@ use Drupal\Tests\commerce\Functional\CommerceBrowserTestBase;
  */
 abstract class StockBrowserTestBase extends CommerceBrowserTestBase {
 
+  protected $profile = 'testing';
+
   use EntityReferenceTestTrait;
 
   /**
