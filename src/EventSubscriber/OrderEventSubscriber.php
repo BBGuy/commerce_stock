9abfcad3926f5f13f98b701074d0d64b7bc7a873
olafkarsten <olafkarsten@352745.no-reply.drupal.org>
2018-03-20 07:38:35 +0100

    Issue #2954152 by olafkarsten: StockTransaction triggered by WorkflowTransition "cancel" can result in wrong stock levels

--- src/EventSubscriber/OrderEventSubscriber.php
+++ src/EventSubscriber/OrderEventSubscriber.php
@@ -122,6 +122,9 @@ class OrderEventSubscriber implements EventSubscriberInterface {
    */
   public function onOrderCancel(WorkflowTransitionEvent $event) {
     $order = $event->getEntity();
+    if ($order->original->getState()->value === 'draft') {
+      return;
+    }
     foreach ($order->getItems() as $item) {
       $entity = $item->getPurchasedEntity();
       if (!$entity) {
