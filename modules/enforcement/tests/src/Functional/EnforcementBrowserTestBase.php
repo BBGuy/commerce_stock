b9972e48f1dc0e67020ef5e18fb516b3c7e40d32
thejacer87 <thejacer87@3225829.no-reply.drupal.org>
2018-11-29 21:44:09 +0100

    Issue #3015818 by thejacer87: enforcement tests

---
+++ modules/enforcement/tests/src/Functional/EnforcementBrowserTestBase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\Tests\commerce_stock_enforcement\Functional;
+
+use Drupal\Tests\commerce_stock\Functional\StockBrowserTestBase;
+
+/**
+ * Defines base class for commerce_stock_enforcement test cases.
+ */
+abstract class EnforcementBrowserTestBase extends StockBrowserTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'commerce_cart',
+    'commerce_stock_enforcement',
+    'commerce_stock_field',
+    'commerce_stock_local',
+  ];
+
+}
