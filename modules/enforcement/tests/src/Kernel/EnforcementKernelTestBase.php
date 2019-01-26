b609f7e046548b6e826664ea098453effcdb98eb
olafkarsten <olafkarsten@352745.no-reply.drupal.org>
2019-01-26 18:47:48 +0100

    Issue #3015818 by thejacer87, olafkarsten: add more enforcement tests. Removing broken kernel test

--- modules/enforcement/tests/src/Kernel/EnforcementKernelTestBase.php
+++ modules/enforcement/tests/src/Kernel/EnforcementKernelTestBase.php
@@ -2,12 +2,12 @@
 
 namespace Drupal\Tests\commerce_stock_enforcement\Kernel;
 
-use Drupal\Tests\commerce_stock\Kernel\CommerceStockKernelTest;
+use Drupal\Tests\commerce_stock\Kernel\CommerceStockKernelTestBase;
 
 /**
  * Defines base class for commerce_stock_enforcement test cases.
  */
-abstract class EnforcementKernelTestBase extends CommerceStockKernelTest {
+abstract class EnforcementKernelTestBase extends CommerceStockKernelTestBase {
 
   /**
    * Modules to enable.
@@ -19,6 +19,12 @@ abstract class EnforcementKernelTestBase extends CommerceStockKernelTest {
     'commerce_stock_enforcement',
     'commerce_stock_field',
     'commerce_stock_local',
+    'commerce_order',
+    'entity_reference_revisions',
+    'profile',
+    'state_machine',
+    'path',
+    'commerce_product',
   ];
 
 }
